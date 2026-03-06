9d622d2720963e3ff74df95ff684d5ba64645cd1
Igor Ryan <igorryanmendesalves@gmail.com>
2026-03-06 17:49:58 -0300

    Correção de bug de update do usuário e implementação de rotas para atualização personalizada

--- src/main/java/com/stcakyforge/matchpoint/controller/UsuarioController.java
+++ src/main/java/com/stcakyforge/matchpoint/controller/UsuarioController.java
@@ -1,10 +1,12 @@
 package com.stcakyforge.matchpoint.controller;
 
+import com.stcakyforge.matchpoint.dtos.request.EmailRequestDto;
 import com.stcakyforge.matchpoint.dtos.request.SenhaRequestDto;
 import com.stcakyforge.matchpoint.dtos.request.UsuarioRequestDto;
 import com.stcakyforge.matchpoint.dtos.response.UsuarioResponseDto;
 import com.stcakyforge.matchpoint.service.UsuarioService;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -31,24 +33,32 @@ public class UsuarioController {
     }
 
     @GetMapping("/{id}")
-    public UsuarioResponseDto pegarUsuario(@PathVariable Long id) {
-        return usuarioService.pegarUsuarioPorId(id);
+    public ResponseEntity<UsuarioResponseDto> pegarUsuario(@PathVariable Long id) {
+        return ResponseEntity.ok(usuarioService.pegarUsuarioPorId(id));
+    }
+
+    @PutMapping("/{id}/edit")
+    public ResponseEntity<Void> atualizarUsuario(@PathVariable Long id, @RequestBody UsuarioRequestDto usuarioRequestDto) {
+        usuarioService.atualizarUsuario(id, usuarioRequestDto);
+        return ResponseEntity.ok().build();
     }
 
     @PutMapping("/{id}/edit/password")
     public ResponseEntity<Void> atualizarSenhaUsuario(@PathVariable Long id, @RequestBody @Valid SenhaRequestDto novaSenha) {
         usuarioService.atualizarSenha(id, novaSenha);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/{id}/edit")
-    public ResponseEntity<UsuarioResponseDto> atualizarUsuario(@PathVariable Long id, @RequestBody @Valid UsuarioRequestDto usuarioRequestDto) {
-        return ResponseEntity.ok(usuarioService.atualizarUsuario(id, usuarioRequestDto));
+
+    @PutMapping("/{id}/edit/email")
+    public ResponseEntity<Void> atualizarEmailUsuario(@PathVariable Long id, @RequestBody @Valid EmailRequestDto novoEmail) {
+        usuarioService.atualizarEmail(id, novoEmail);
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
         usuarioService.deleteUsuario(id);
-        return  ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.ok().build();
     }
 }
